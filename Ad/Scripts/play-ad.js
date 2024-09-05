b31f4b55c51b5eebb11d18c41e352ad74a2f49cd
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2024-09-04 23:05:01 -0700

    frame 2a done

--- Ad/Scripts/play-ad.js
+++ Ad/Scripts/play-ad.js
@@ -1,33 +1,29 @@
 const buildingImg = document.querySelector(".building");
 const logo = document.querySelector(".logo");
+const redBG = document.querySelector(".red-banner");
+const txt2A = document.querySelector(".txt2A");
+const txt2B = document.querySelector(".txt2B");
+const frame2 = document.querySelector(".frame-2");
 
 window.addEventListener("load", reveal);
 
 function reveal() {
+    //create timeline object
     const tl = gsap.timeline({
         default: {
-            ease: "power2",
+            ease: "power3",
         }, // <- these commas are added by "prettier extension"
     });
 
-    //Frame 1
-    tl.from(logo, { scale: 0.3, autoAlpha: 0, duration: 1 }).to(logo, {
-        autoAlpha: 0,
-        duration: 1,
-        delay: 1.5,
-    });
+    //====== Frame 1 ======
+    tl.from(logo, { scale: 0.3, autoAlpha: 0, duration: 1 }) //fade in logo
+        .to(logo, { autoAlpha: 0, duration: 0.6, delay: 1 }) // fade out logo
+        .to(redBG, { y: 140, duration: 0.8 }, "-=0.25"); //slide redBG down. start this animation 0.25 seconds early
 
-    //Frame 2a
-
-    // timeline
-    //     .from(title, { autoAlpha: 0, y: -50, delay: 0.2 })
-    //     .to(verticalLine, { height: 200 }, "-=0.2")
-    //     .from(grapesLogo, { autoAlpha: 0, y: -50 }, "-=0.2")
-    //     .from(homeBtn, { autoAlpha: 0, y: -50 }, "-=0.2")
-    //     .from(
-    //         navLinks,
-    //         { autoAlpha: 0, y: -50, duration: 0.4, stagger: 0.1 },
-    //         "-=0.2"
-    //     )
-    //     .from(imgsNav, { autoAlpha: 0, y: -50 });
+    //====== Frame 2a ======
+    tl.from(buildingImg, { scale: 2.3, y: -300, duration: 6 }, "-=1.5") //start from the left //slide redBG down. start this animation 1 second early
+        .from(txt2A, { x: -70, duration: 0 })
+        .to(txt2A, { x: 0, autoAlpha: 1, duration: 1 }, "-=5.5");
+    // .to(redBG, { y: 90, duration: 0.8 }, "-=3.25") //slide redBG up. start this animation 0.25 seconds early
+    // .to(frame2, { y: 90, duration: 0.8 }, "-=3.25");
 }
